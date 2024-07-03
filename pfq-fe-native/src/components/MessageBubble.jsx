fdb8ea6cc9e170cff17492766b0bcf00dc9d77e8
rambatinoo <barrywjames1990@gmail.com>
2024-07-03 11:56:32 +0100

    adds functionality for messages to auto scroll when a new mesaage appears

--- pfq-fe-native/src/components/MessageBubble.jsx
+++ pfq-fe-native/src/components/MessageBubble.jsx
@@ -3,12 +3,16 @@ import { View, Text, StyleSheet } from "react-native";
 export const MessageBubble = ({ body, isSender, timestamp }) => {
   return (
     <View>
-    <View
-      style={[styles.container, isSender ? styles.sender : styles.receiver]}
-    >
-      <Text style={isSender? styles.messageText : styles.messageTextAdmin}>{body}</Text>
-    </View>
-    <Text style={isSender ? styles.timestamp : styles.timestampAdmin}>{timestamp}</Text>
+      <View
+        style={[styles.container, isSender ? styles.sender : styles.receiver]}
+      >
+        <Text style={isSender ? styles.messageText : styles.messageTextAdmin}>
+          {body}
+        </Text>
+      </View>
+      <Text style={isSender ? styles.timestamp : styles.timestampAdmin}>
+        {timestamp}
+      </Text>
     </View>
   );
 };
@@ -20,23 +24,28 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     maxWidth: "100%",
   },
-  timestamp : {
+  timestamp: {
     padding: 10,
-    marginVertical: 0,
+    marginBottom: 10,
     marginHorizontal: 10,
     borderRadius: 10,
     maxWidth: "80%",
     marginLeft: 250,
   },
-  timestampAdmin : {
+  timestampAdmin: {
     padding: 10,
     marginVertical: 0,
     marginHorizontal: 10,
     borderRadius: 10,
     maxWidth: "80%",
   },
-  sender: { backgroundColor: "#007AFF", alignSelf: "flex-end" },
-  receiver: { backgroundColor: "#E5E5EA", marginRight: 200},
+  sender: {
+    backgroundColor: "#007AFF",
+    alignSelf: "flex-end",
+    marginBottom: -8,
+    marginTop: -5,
+  },
+  receiver: { backgroundColor: "#E5E5EA", marginRight: 200 },
   messageText: { color: "#fff" },
   messageTextAdmin: { color: "#000" },
 });
