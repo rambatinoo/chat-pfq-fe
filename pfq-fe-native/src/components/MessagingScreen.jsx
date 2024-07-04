ae5f01e49e9c30ea2afe4192350168d3fbd1feb4
rambatinoo <barrywjames1990@gmail.com>
2024-07-04 16:45:58 +0100

    now renders the message styling based on if 'admin' is the sender

--- pfq-fe-native/src/components/MessagingScreen.jsx
+++ pfq-fe-native/src/components/MessagingScreen.jsx
@@ -21,7 +21,7 @@ export const MessagingScreen = ({ username, socket }) => {
   useEffect(() => {
     const getMessageThread = async () => {
       try {
-        const messages = await getRequest("messages");
+        const messages = await getRequest("messages", { username });
         const updatedMessages = messages.map((message) => {
           if (message.from === username.toLowerCase()) {
             message.sender = true;
@@ -84,6 +84,7 @@ export const MessagingScreen = ({ username, socket }) => {
             data={filterMessagesByUsername(messages, username)}
             renderItem={({ item }) => (
               <MessageBubble
+                to={item.to}
                 body={item.body}
                 isSender={item.sender}
                 timestamp={item.created_at}
@@ -179,126 +180,3 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
 });
-
-// import { useEffect, useState } from "react";
-// import {
-//   FlatList,
-//   TextInput,
-//   View,
-//   TouchableOpacity,
-//   Text,
-//   StyleSheet,
-// } from "react-native";
-// import { MessageBubble } from "./MessageBubble";
-
-// export const MessagingScreen = ({ username, socket }) => {
-//   const [messages, setMessages] = useState([]);
-//   const [message, setMessage] = useState("");
-//   const [tableNum, setTableNum] = useState("");
-
-//   useEffect(() => {
-//     socket.on('message', (msg) => {
-//       setMessages((prevMessages) => [...prevMessages, msg])
-//     })
-//   }, [])
-
-//   const sendMessage = () => {
-//     if (body.trim() !== "") {
-//       socket.emit("send-customer-message", {
-//         body,
-//         tableNum,
-//         username,
-//         sender: true,
-//       });
-//       setBody("");
-//       setTableNum("");
-//     }
-//   };
-//   return (
-//     <View style={styles.container}>
-//       <FlatList
-//         data={messages}
-//         renderItem={({ item }) => (
-//           <MessageBubble body={item.body} isSender={item.sender} timestamp={item.created_at}/>
-//         )}
-//         keyExtractor={(item, index) => index.toString()}
-//         contentContainerStyle={styles.messageContainer}
-//       />
-//       <View style={styles.messageInputContainer}>
-//         <TextInput
-//           style={styles.messageInput}
-//           placeholder="Message"
-//           onChangeText={(text) => setBody(text)}
-//           value={body}
-//           multiline={true}
-//           scrollEnabled={true}
-//         />
-//         <TouchableOpacity style={styles.sendButton} onPress={sendMessage}>
-//           <Text style={styles.sendButtonText}>Send</Text>
-//         </TouchableOpacity>
-//       </View>
-//       <View style={styles.middleContainer}>
-//         <TextInput
-//           style={styles.tableInput}
-//           placeholder="Table Number"
-//           onChangeText={(text) => setTableNum(text)}
-//           value={tableNum}
-//         />
-//       </View>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "space-between",
-//   },
-//   messageContainer: {
-//     paddingHorizontal: 10,
-//     paddingVertical: 20,
-//   },
-//   messageInputContainer: {
-//     flexDirection: "row",
-//     alignItems: "center",
-//     padding: 10,
-//     borderTopWidth: 1,
-//     borderColor: "#ccc",
-//     backgroundColor: "#fff",
-//   },
-//   messageInput: {
-//     borderWidth: 1,
-//     borderColor: "#ccc",
-//     borderRadius: 20,
-//     paddingVertical: 10,
-//     paddingHorizontal: 15,
-//     maxHeight: 80,
-//     backgroundColor: "#f0f0f0",
-//     flex: 1,
-//     marginRight: 10,
-//   },
-//   sendButton: {
-//     justifyContent: "center",
-//     alignItems: "center",
-//     backgroundColor: "#007AFF",
-//     borderRadius: 20,
-//     paddingVertical: 10,
-//     paddingHorizontal: 15,
-//   },
-//   sendButtonText: {
-//     color: "#fff",
-//     fontWeight: "bold",
-//   },
-//   middleContainer: {
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-//   tableInput: {
-//     height: 40,
-//     width: '80%',
-//     borderWidth: 1,
-//     borderColor: "#ccc",
-//     borderRadius: 20,
-//     paddingHorizontal: 10,
-//   },
-// });
