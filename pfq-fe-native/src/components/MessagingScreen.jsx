8e8c3f17f5353a161a75c96d13aa109076543c6d
rambatinoo <barrywjames1990@gmail.com>
2024-07-08 15:13:08 +0100

    styling for hover over message preview - need to look at colours

--- pfq-fe-native/src/components/MessagingScreen.jsx
+++ pfq-fe-native/src/components/MessagingScreen.jsx
@@ -65,7 +65,6 @@ export const MessagingScreen = ({ username, socket, setUsername }) => {
 
   const sendMessage = () => {
     const date = new Date().toISOString();
-    console.log(date, typeof date);
     if (body.trim() !== "") {
       socket.emit("send-customer-message", {
         body,
