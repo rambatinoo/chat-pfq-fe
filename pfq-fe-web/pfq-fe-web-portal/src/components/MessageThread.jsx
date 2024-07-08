3a3c26e6f939e7b94d33404c0293e9251321bfdb
Liam Houghton <liamhoughton@Liams-MBP.northcoders.com>
2024-07-08 10:56:31 +0100

    fix scrolling behaviour on new message received

--- pfq-fe-web/pfq-fe-web-portal/src/components/MessageThread.jsx
+++ pfq-fe-web/pfq-fe-web-portal/src/components/MessageThread.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useRef } from "react";
 import { Message } from "./Message";
 
 export function MessageThread({ conversationMessages }) {
-    return <div id="message-thread">
+
+  const messageThreadRef = useRef(null)
+
+  useEffect(() => {
+    if (messageThreadRef.current) {
+      messageThreadRef.current.scrollTop = messageThreadRef.current.scrollHeight
+    }
+  }, [conversationMessages])
+
+    return <div id="message-thread" ref={messageThreadRef}>
         {conversationMessages.map((msg) => {
           return (
               <div>
