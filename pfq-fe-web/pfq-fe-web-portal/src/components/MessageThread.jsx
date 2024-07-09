5bba09a3a1beea1a06dda2c47ae902f0bb0b65c4
rambatinoo <barrywjames1990@gmail.com>
2024-07-09 13:51:48 +0100

    fixes issue with admin not being able to reply to users

--- pfq-fe-web/pfq-fe-web-portal/src/components/MessageThread.jsx
+++ pfq-fe-web/pfq-fe-web-portal/src/components/MessageThread.jsx
@@ -2,22 +2,24 @@ import { useEffect, useRef } from "react";
 import { Message } from "./Message";
 
 export function MessageThread({ conversationMessages }) {
-
-  const messageThreadRef = useRef(null)
+  const messageThreadRef = useRef(null);
 
   useEffect(() => {
     if (messageThreadRef.current) {
-      messageThreadRef.current.scrollTop = messageThreadRef.current.scrollHeight
+      messageThreadRef.current.scrollTop =
+        messageThreadRef.current.scrollHeight;
     }
-  }, [conversationMessages])
+  }, [conversationMessages]);
 
-    return <div id="message-thread" ref={messageThreadRef}>
-        {conversationMessages.map((msg) => {
-          return (
-              <div>
-                <Message msg={msg}/>
-              </div>
-          );
-        })}
-      </div>
+  return (
+    <div id="message-thread" ref={messageThreadRef}>
+      {conversationMessages.map((msg) => {
+        return (
+          <div key={msg.created_at}>
+            <Message msg={msg} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
